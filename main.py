1eecc4f0d982be478c94416899d15e479fed9c04
projectperfect <jprfct@protonmail.com>
2022-07-28 17:34:35 -0500

    import the logo from hangman_art.py and print it as the start of the game.

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from hangman_words import word_list
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
+from hangman_art import logo
+print(logo)
+
 from hangman_art import stages
     print(stages[lives])
